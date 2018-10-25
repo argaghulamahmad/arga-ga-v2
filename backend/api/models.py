ab4af0d6e0a8446a87160105d09d3ae92431a103
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-10-25 11:53:16 +0700

    Update models

--- backend/api/models.py
+++ backend/api/models.py
@@ -118,10 +118,17 @@ class Guest(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     profile_picture = models.URLField()
+    visit_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s' % self.name
 
 
 class Workflow(models.Model):
     content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s' % self.content
 
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
